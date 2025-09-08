03da9d60f17159d44ea110692036161a720c84a0
Brenda232 <brendaest23u@gmail.com>
2025-09-08 11:00:20 -0600

    se modifico

--- includes/header.php
+++ includes/header.php
@@ -15,25 +15,15 @@
 
 <!-- Navbar -->
 <!-- Navbar -->
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="index.php">Mi Proyecto</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
-        <li class="nav-item"><a class="nav-link" href="about.php">Nosotros</a></li>
-        <li class="nav-item"><a class="nav-link" href="contact.php">Contacto</a></li>
-
-        <?php if(isset($_SESSION['usuario'])): ?>
-          <li class="nav-item"><a class="nav-link">👤 <?php echo $_SESSION['usuario']; ?></a></li>
-          <li class="nav-item"><a class="nav-link" href="logout.php">Salir</a></li>
-        <?php else: ?>
-          <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-          <li class="nav-item"><a class="nav-link" href="register.php">Registro</a></li>
-        <?php endif; ?>
-      </ul>
-    </div>
+    <a class="navbar-brand" href="#">
+      <img src="img/hulux.png" alt="Logo" width="60" height="24">
+    </a>
   </div>
 </nav>
+
+
 
 

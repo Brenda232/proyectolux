0262596e383e6d9b67f4db38b559b3107b347e8b
Brenda232 <brendaest23u@gmail.com>
2025-09-08 09:51:10 -0600

    el header se modifico

--- includes/header.php
+++ includes/header.php
@@ -14,18 +14,26 @@
 <body>
 
 <!-- Navbar -->
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand" href="index.php">Mi Proyecto</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
         <li class="nav-item"><a class="nav-link" href="about.php">Nosotros</a></li>
         <li class="nav-item"><a class="nav-link" href="contact.php">Contacto</a></li>
+
+        <?php if(isset($_SESSION['usuario'])): ?>
+          <li class="nav-item"><a class="nav-link">👤 <?php echo $_SESSION['usuario']; ?></a></li>
+          <li class="nav-item"><a class="nav-link" href="logout.php">Salir</a></li>
+        <?php else: ?>
+          <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+          <li class="nav-item"><a class="nav-link" href="register.php">Registro</a></li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
 </nav>
+
+
